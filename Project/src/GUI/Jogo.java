bd6fe80374f6a7377f137b2d427a7d2406db277c
Thiago Vieira <thiagovieiraas@gmail.com>
2015-12-14 15:58:16 -0200

    Added a menu to the game

--- Project/src/GUI/Jogo.java
+++ Project/src/GUI/Jogo.java
@@ -14,7 +14,7 @@ import java.awt.Graphics;
 import java.awt.event.KeyListener;
 import java.awt.image.BufferedImage;
 
-public class Jogo extends JFrame {
+public class Jogo extends javax.swing.JFrame {
 
     private final int VAZIO = 0;
     private final int TIJOLO = 1;
@@ -26,6 +26,11 @@ public class Jogo extends JFrame {
     private JPanel contentPane;
     private Mapa mapa;
     private KeyListener listener;
+    
+    private javax.swing.JMenu jMenuItem1;
+    private javax.swing.JMenu jMenuItem2;
+    private javax.swing.JMenu jMenuItem3;
+    private javax.swing.JMenuBar jMenuBar;
 
     public void IniciaJogo() {
         Jogo frame = new Jogo();
@@ -44,7 +49,48 @@ public class Jogo extends JFrame {
         mapa.setJogador(Jogador.getInstance("Pedro"));
         listener = new MKeyListener(mapa, this);
         this.addKeyListener(listener);
+        
+        
+        jMenuBar = new javax.swing.JMenuBar();
+        jMenuItem1 = new javax.swing.JMenu();
+        jMenuItem2 = new javax.swing.JMenu();
+        jMenuItem3 = new javax.swing.JMenu();
+        
+        
+        jMenuItem3 = new javax.swing.JMenu();
+        jMenuItem3.setText("Nivel 1");
+        jMenuItem3.setFocusable(false);
+        jMenuItem3.setRolloverEnabled(false);
+        jMenuBar.add(jMenuItem3);
+        
+        
+        jMenuItem1.setText("Voltar Nivel");
+        jMenuItem1.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                if(mapa instanceof Mapa02) {
+                    mudaFase01();
+                }
+                else if(mapa instanceof Mapa03) {
+                    mudaFase02();
+                }
+                else if(mapa instanceof Mapa04) {
+                    mudaFase03();
+                }
+            }
+        });
+        jMenuBar.add(jMenuItem1);
+        
+        jMenuItem2.setText("Sair");
+        jMenuItem2.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                System.exit(0);
+            }
+        });
+        jMenuBar.add(jMenuItem2);
 
+        setJMenuBar(jMenuBar);
+        
+        
         atualizaGUI();
     }
 
@@ -83,12 +129,23 @@ public class Jogo extends JFrame {
         }
     }
 
+    
+    public void mudaFase01() {
+        mapa = new Mapa01();
+        removeKeyListener(listener);
+        listener = new MKeyListener(mapa, this);
+        addKeyListener(listener);
+        atualizaGUI();
+        jMenuItem3.setText("Nivel 1");
+    }
+    
     public void mudaFase02() {
         mapa = new Mapa02();
         removeKeyListener(listener);
         listener = new MKeyListener(mapa, this);
         addKeyListener(listener);
         atualizaGUI();
+        jMenuItem3.setText("Nivel 2");
     }
 
     public void mudaFase03() {
@@ -97,6 +154,7 @@ public class Jogo extends JFrame {
         listener = new MKeyListener(mapa, this);
         addKeyListener(listener);
         atualizaGUI();
+        jMenuItem3.setText("Nivel 3");
     }
 
     public void mudaFase04() {
@@ -105,6 +163,7 @@ public class Jogo extends JFrame {
         listener = new MKeyListener(mapa, this);
         addKeyListener(listener);
         atualizaGUI();
+        jMenuItem3.setText("Nivel 4");
     }
 
     public void mudaFase05() {
@@ -113,6 +172,7 @@ public class Jogo extends JFrame {
         listener = new MKeyListener(mapa, this);
         addKeyListener(listener);
         atualizaGUI();
+        jMenuItem3.setText("Nivel 5");
     }
 
     public void showRedPlane() {
